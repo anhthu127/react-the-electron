c5161ab5fa87068479e989e5a789d374bd159fda
mchiozzi <mchiozzi95@gmail.com>
2020-11-24 12:43:33 -0300

    remove temporaly the modal fade effect until material-ui new release

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 
 import Modal from "@material-ui/core/Modal";
-import Backdrop from "@material-ui/core/Backdrop";
-import Fade from "@material-ui/core/Fade";
 
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -31,16 +29,13 @@ export default function StdModal(props) {
       open={props.open}
       onClose={props.handleClose}
       closeAfterTransition
-      BackdropComponent={Backdrop}
       BackdropProps={{
         timeout: 500,
       }}
     >
-      <Fade in={props.open}>
-        <div className={classes.paper}>
-          <props.component />
-        </div>
-      </Fade>
+      <div className={classes.paper}>
+        <props.component />
+      </div>
     </Modal>
   );
 }
